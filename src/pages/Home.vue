92a9b3ef7b69fc282161f0c7e55baff4a7d907fd
Andres M. Prieto <andresprieto2703@gmail.com>
2022-11-23 13:05:19 -0500

    Home page
    Just adding test pages which is going to be the "Home" page in future
    commits

---
+++ src/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-main>
+    <Router />
+    <Bar />
+    <NotFound />
+  </v-main>
+</template>
+
+<script>
+import NotFound from "@components/NotFound.vue";
+import Router from "@templates/Router.vue";
+import Bar from "@templates/Bar.vue";
+
+export default {
+  name: "Home",
+  components: {
+    NotFound,
+    Router,
+    Bar,
+  },
+};
+</script>
