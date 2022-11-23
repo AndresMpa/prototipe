e3841af6abc017734140b0cad2104d1b7211b4c9
Andres M. Prieto <andresprieto2703@gmail.com>
2022-11-23 12:18:48 -0500

    Router
    Adding app routes (No pages yet)

---
+++ src/router/index.js
@@ -0,0 +1,108 @@
+import {
+  createRouter,
+  createWebHistory,
+  isNavigationFailure,
+  NavigationFailureType,
+} from "vue-router";
+import store from "@store/index.js";
+
+import Home from "@pages/Home";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: { requiresAuth: true, transition: "slide-left" },
+    children: [
+      {
+        name: "Achievements",
+        path: "/achievements",
+        meta: { requiresAuth: true, transition: "slide-left" },
+        component: () => import(/* webpackChunkName: "Home" */ "@pages/Home"),
+      },
+      {
+        name: "Progress",
+        path: "/progress",
+        meta: { requiresAuth: true, transition: "slide-left" },
+        component: () => import(/* webpackChunkName: "Home" */ "@pages/Home"),
+      },
+      {
+        name: "Patients",
+        path: "/patients",
+        meta: { requiresAuth: true, transition: "slide-left" },
+        component: () => import(/* webpackChunkName: "Home" */ "@pages/Home"),
+      },
+    ],
+  },
+  {
+    name: "Login",
+    path: "/login",
+    meta: { requiresAuth: false, transition: "slide-left" },
+    component: () => import(/* webpackChunkName: "Home" */ "@pages/Home"),
+    children: [
+      {
+        name: "Recovery",
+        path: "/recovery",
+        meta: { requiresAuth: false, transition: "slide-left" },
+        component: () => import(/* webpackChunkName: "Home" */ "@pages/Home"),
+      },
+      {
+        name: "Create",
+        path: "/create-account",
+        meta: { requiresAuth: false, transition: "slide-left" },
+        component: () => import(/* webpackChunkName: "Home" */ "@pages/Home"),
+      },
+    ],
+  },
+  {
+    name: "Landing",
+    path: "/landing",
+    meta: { requiresAuth: false, transition: "slide-left" },
+    component: () => import(/* webpackChunkName: "Home" */ "@pages/Home"),
+    children: [
+      {
+        name: "Purchase",
+        path: "/purchase",
+        meta: { requiresAuth: false, transition: "slide-left" },
+        component: () => import(/* webpackChunkName: "Home" */ "@pages/Home"),
+      },
+      {
+        name: "Bill",
+        path: "/bill",
+        meta: { requiresAuth: false, transition: "slide-left" },
+        component: () => import(/* webpackChunkName: "Home" */ "@pages/Home"),
+      },
+    ],
+  },
+  {
+    name: "NotFound",
+    path: "/:pathMatch(.*)*",
+    meta: { requiresAuth: false, transition: "slide-left" },
+    component: () => import(/* webpackChunkName: "Home" */ "@pages/Home"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
+});
+
+router.beforeEach((to, from) => {
+  if (to.meta.requiresAuth && !store.state.auth.isLoggedIn) {
+    return {
+      path: "/login",
+    };
+  }
+});
+
+//router.push({ name: 'user'})
+
+export default router;
