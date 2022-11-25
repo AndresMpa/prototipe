7fb1f60d7b47b86c5c1f251a2b6f2a544b1f6da2
Andres M. Prieto <andresprieto2703@gmail.com>
2022-11-25 02:40:28 -0500

    Refactor
    Using Home as wrapper to reduce useless code

---
+++ src/pages/Progress.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Stats />
+  </div>
+</template>
+
+<script>
+import Stats from "@components/Achievements/Stats.vue";
+
+export default {
+  name: "Progress",
+  components: {
+    Stats,
+  },
+};
+</script>
