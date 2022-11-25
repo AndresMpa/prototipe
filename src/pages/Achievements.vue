5d62054f3a0987d2cdf8c0c631dcedec6cb62c55
Andres M. Prieto <andresprieto2703@gmail.com>
2022-11-25 02:39:15 -0500

    Badges stuff
    Adding badges

---
+++ src/pages/Achievements.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Trophy />
+  </div>
+</template>
+
+<script>
+import Trophy from "@templates/Trophy.vue";
+
+export default {
+  name: "Achievements",
+  components: {
+    Trophy,
+  },
+};
+</script>
---
+++ src/templates/Trophy.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <BadgesStore :name="intro.unlocked" :store="unlockedBadges" />
+    <BadgesStore :name="intro.locked" :store="lockedBadges" />
+  </div>
+</template>
+
+<script>
+import BadgesStore from "@containers/BadgesStore.vue";
+
+export default {
+  name: "Trophy",
+  data: () => ({
+    intro: {
+      unlocked: {
+        title: "Logros obtenidos",
+        hint: "Estos son los logros que tienes, esfuerzate por tenerlos todos",
+      },
+      locked: {
+        title: "Lista de logros",
+        hint: "Aquí puedes ver todos los logros que puedes tener",
+      },
+    },
+    unlockedBadges: [
+      {
+        description: "Salir a hacer senderismo al menos 5 veces por una semana",
+        img: "https://picsum.photos/500/300?image=17",
+        name: "Sendero zen",
+      },
+      {
+        description: "Salir a hacer camping un fin de semana por más de 2 días",
+        img: "https://picsum.photos/500/300?image=15",
+        name: "¡Campamento!",
+      },
+      {
+        description: "Ingresa a tu cuentra de Heropu",
+        img: "https://picsum.photos/500/300?image=1",
+        name: "Empieza el tratamiento",
+      },
+    ],
+    lockedBadges: [
+      {
+        description: "Ir a la playa a nadar una tarde",
+        img: "https://picsum.photos/500/300?image=16",
+        name: "Oceano de pensamientos",
+      },
+      {
+        description: "Adopta o cultiva una planta en una maseta",
+        img: "https://picsum.photos/500/300?image=18",
+        name: "Amiga de verder ramas",
+      },
+      {
+        description: "Abrazar un arbol más grande que el ancho de los brazos",
+        img: "https://picsum.photos/500/300?image=19",
+        name: "Señor árbol",
+      },
+      {
+        description: "Mantener tu lugar de descanso ordenado por una semana",
+        img: "https://picsum.photos/500/300?image=20",
+        name: "Un lugar en paz",
+      },
+    ],
+  }),
+  components: {
+    BadgesStore,
+  },
+};
+</script>
+
+<style></style>
