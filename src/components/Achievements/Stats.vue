30b39845bce7d4e8f78460a1d98ee097e5b4e694
Andres M. Prieto <andresprieto2703@gmail.com>
2022-11-25 00:17:18 -0500

    Split
    Splitting achievements components in two different folders to have make
    this more maintainable

---
+++ src/components/Achievements/Stats.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="ma-2 rounded-lg">
+    <Bar :chart-options="chartOptions" :chart-data="chartData" />
+  </div>
+</template>
+
+<script>
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Legend,
+  Tooltip,
+  BarElement,
+  LinearScale,
+  CategoryScale,
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+
+export default {
+  name: "Stats",
+  data() {
+    return {
+      chartData: {
+        labels: ["January", "February", "March"],
+        datasets: [
+          {
+            label: "Progreso del mes",
+            backgroundColor: "#78f",
+            data: [23, 20, 25],
+          },
+        ],
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: true,
+      },
+    };
+  },
+  components: { Bar },
+};
+</script>
+
+<style scoped>
+.stats {
+  z-index: 10;
+}
+</style>
