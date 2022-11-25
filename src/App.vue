cb402f9168e123c38f8d85eef0ac5166e8b092fd
Andres M. Prieto <andresprieto2703@gmail.com>
2022-11-25 02:40:03 -0500

    MLD
    Removing unused code

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app id="inspire">
-    <router-view v-slot="{ Component, route }">
-      <transition :name="route.meta.transition || 'fade'">
-        <component :is="Component" :key="route.path" />
-      </transition>
-    </router-view>
+    <router-view />
   </v-app>
 </template>
 
